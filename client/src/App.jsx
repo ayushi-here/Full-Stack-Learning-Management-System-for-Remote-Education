115387fa7cb3e67932a7ddec71be3c7408b09289
Ayushi <believerayushi@gmail.com>
2026-01-29 09:50:57 +0530

    Initial setup: MERN LMS frontend + backend scaffold

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Button className='bg-green-400 hover:bg-green-900'>Check</Button>
+    </>
+  )
+}
+
+export default App
