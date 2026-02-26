057cd967531b3976cf3e7ccfeb4b8a7624b4467d
Ayushi <believerayushi@gmail.com>
2026-02-26 17:20:01 +0530

    fixed issue (version was the prob)

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const authRoutes = require("./routes/auth-routes/index");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 const MONGO_URI = process.env.MONGO_URI;
 
 app.use(cors({
